0031130c2e6d645bb74f8b66566561ce4aa6821d
mrousavy <mrousavy@donotreply.github.com>
2017-09-08 17:27:45 +0200

    Fix architectures for floating precision

--- CppNeuralNetwork/Functions.cpp
+++ CppNeuralNetwork/Functions.cpp
@@ -2,28 +2,30 @@
 #include "Functions.h"
 #include <cmath>
 
-#if _X86_ || _M_IX86 || _M_IA64 || _M_ARM
+#if _X86_ || _M_IX86 || _M_IA64 || _M_ARM || _WIN64 || _WIN32
 // On CPUs with DOUBLE_PRECISION Hardware-Implementation:
-	const double EulerConstant = std::exp(1.0);
-	/// <summary>
-	/// Squash a value with the Sigmoid
-	/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
-	/// double
-	/// </summary>
-	double Squash(double value) {
-		double exp = std::pow(EulerConstant, value);
-		return exp / (exp + 1);
-	}
+const double EulerConstant = std::exp(1.0);
+/// <summary>
+/// Squash a value with the Sigmoid
+/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
+/// double
+/// </summary>
+double Squash(double value)
+{
+	double exp = std::pow(EulerConstant, value);
+	return exp / (exp + 1);
+}
 #else
 // On CPUs with DOUBLE_PRECISION Software-Implementation:
-	const float EulerConstant = std::exp(1.0);
-	/// <summary>
-	/// Squash a value with the Sigmoid
-	/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
-	/// floats (Min: -63 | Max: 64)
-	/// </summary>
-	float Squash(float value) {
-		float exp = std::pow(EulerConstant, value);
-		return exp / (exp + 1);
-	}
+const float EulerConstant = std::exp(1.0);
+/// <summary>
+/// Squash a value with the Sigmoid
+/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
+/// floats (Min: -63 | Max: 64)
+/// </summary>
+float Squash(float value)
+{
+	float exp = std::pow(EulerConstant, value);
+	return exp / (exp + 1);
+}
 #endif
--- CppNeuralNetwork/Functions.h
+++ CppNeuralNetwork/Functions.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#if _X86_ || _M_IX86 || _M_IA64 || _M_ARM
+#if _X86_ || _M_IX86 || _M_IA64 || _M_ARM || _WIN64 || _WIN32
 // On CPUs with DOUBLE_PRECISION Hardware-Implementation:
 	/// <summary>
 	/// Squash a value with the Sigmoid
 	/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
 	/// doubles
 	/// </summary>
-	double Squash(double);
+double Squash(double);
 #else
 // On CPUs with DOUBLE_PRECISION Software-Implementation:
 	/// <summary>
@@ -15,5 +15,5 @@
 	/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
 	/// floats (Min: -63 | Max: 64)
 	/// </summary>
-	float Squash(float);
+float Squash(float);
 #endif
