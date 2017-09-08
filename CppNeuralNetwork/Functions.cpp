40b4c2f8743b7b7639c095d2349db50f561566d3
mrousavy <mrousavy@donotreply.github.com>
2017-09-08 17:51:05 +0200

    Multiple sigmoid functions

--- CppNeuralNetwork/Functions.cpp
+++ CppNeuralNetwork/Functions.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 #if _X86_ || _M_IX86 || _M_IA64 || _M_ARM || _WIN64 || _WIN32
+
 // On CPUs with DOUBLE_PRECISION Hardware-Implementation:
 const double EulerConstant = std::exp(1.0);
 /// <summary>
@@ -12,12 +13,15 @@ const double EulerConstant = std::exp(1.0);
 /// </summary>
 double Squash(double value)
 {
-	double exp = std::pow(EulerConstant, value);
-	return exp / (exp + 1);
+	double expP = std::pow(EulerConstant, value);
+	double expN = std::pow(EulerConstant, -value);
+	return (expP - expN) / (expP + expN);
 }
+
 #else
+
 // On CPUs with DOUBLE_PRECISION Software-Implementation:
-const float EulerConstant = std::exp(1.0);
+const float EulerConstant = std::exp((float)1.0);
 /// <summary>
 /// Squash a value with the Sigmoid
 /// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
@@ -25,7 +29,22 @@ const float EulerConstant = std::exp(1.0);
 /// </summary>
 float Squash(float value)
 {
-	float exp = std::pow(EulerConstant, value);
-	return exp / (exp + 1);
+	float expP = std::pow(EulerConstant, value);
+	float expN = std::pow(EulerConstant, -value);
+	return (expP - expN) / (expP + expN);
 }
+
 #endif
+
+
+// LOGISTIC:
+//   float exp = std::pow(EulerConstant, value);
+//   return exp / (exp + 1);
+
+// HYPERBOLIC TAN:
+//   double expP = std::pow(EulerConstant, value);
+//   double expN = std::pow(EulerConstant, -value);
+//   return (expP - expN) / (expP + expN);
+
+// SPECIFIC ALGEBRAIC
+//   return (value) / std::sqrt(1 + std::pow(value, 2));
