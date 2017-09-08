e4139555e20891cca806848a46e8561b6719df61
mrousavy <marcrousavy@users.donotreply.github.com>
2017-09-08 10:00:05 +0200

    Add Sigmoid function

--- CppNeuralNetwork/Functions.cpp
+++ CppNeuralNetwork/Functions.cpp
@@ -1,7 +1,14 @@
 #include "stdafx.h"
 #include "Functions.h"
+#include <cmath>
+const double EulerConstant = std::exp(1.0);
 
-
+/// <summary>
+/// Squash a value with the Sigmoid
+/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
+/// floats (Min: -63 | Max: 64)
+/// </summary>
 float Squash(float value) {
-
+	float exp = std::pow(EulerConstant, value);
+	return exp / (exp + 1);
 }
--- CppNeuralNetwork/Functions.h
+++ CppNeuralNetwork/Functions.h
@@ -1,4 +1,9 @@
 #pragma once
 
+/// <summary>
+/// Squash a value with the Sigmoid
+/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
+/// floats (Min: -63 | Max: 64)
+/// </summary>
 float Squash(float);
 
