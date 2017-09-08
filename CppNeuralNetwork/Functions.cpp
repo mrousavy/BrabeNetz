1461bac26bb17a8fbeb08a317fd288183c148ffe
mrousavy <marcrousavy@users.donotreply.github.com>
2017-09-08 11:48:29 +0200

    Adjust sigmoid function for x86 CPUs

--- CppNeuralNetwork/Functions.cpp
+++ CppNeuralNetwork/Functions.cpp
@@ -1,14 +1,29 @@
 #include "stdafx.h"
 #include "Functions.h"
 #include <cmath>
-const double EulerConstant = std::exp(1.0);
 
-/// <summary>
-/// Squash a value with the Sigmoid
-/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
-/// floats (Min: -63 | Max: 64)
-/// </summary>
-float Squash(float value) {
-	float exp = std::pow(EulerConstant, value);
-	return exp / (exp + 1);
-}
+#if _WIN64 || _WIN32
+// On x86 CPUs:
+	const double EulerConstant = std::exp(1.0);
+	/// <summary>
+	/// Squash a value with the Sigmoid
+	/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
+	/// double
+	/// </summary>
+	double Squash(double value) {
+		double exp = std::pow(EulerConstant, value);
+		return exp / (exp + 1);
+	}
+#else
+// On other CPUs:
+	const float EulerConstant = std::exp(1.0);
+	/// <summary>
+	/// Squash a value with the Sigmoid
+	/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
+	/// floats (Min: -63 | Max: 64)
+	/// </summary>
+	float Squash(float value) {
+		float exp = std::pow(EulerConstant, value);
+		return exp / (exp + 1);
+	}
+#endif
--- CppNeuralNetwork/Functions.h
+++ CppNeuralNetwork/Functions.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#if _WIN64 || _WIN32
+/// <summary>
+/// Squash a value with the Sigmoid
+/// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
+/// doubles
+/// </summary>
+double Squash(double);
+#else
 /// <summary>
 /// Squash a value with the Sigmoid
 /// function (https://en.wikipedia.org/wiki/Sigmoid_function) using
 /// floats (Min: -63 | Max: 64)
 /// </summary>
 float Squash(float);
-
+#endif
