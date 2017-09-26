c65b56281518adfbc7d3287ad5afb9cedd8b740d
Marc Rousavy <marcrousavy@hotmail.com>
2017-09-26 13:22:04 +0200

    Trainer Comments

--- CppNeuralNetwork/Trainer.cpp
+++ CppNeuralNetwork/Trainer.cpp
@@ -19,7 +19,11 @@ void Trainer::TrainXor()
 	int size = 2;
 	double* values = new double[size] { 1, 1 };
 	int output = 1;
-	_net->Train(values, size, output);
+
+	cout << "{ 1, 1 }: ";
+	// TODO: TRAIN RETURNS ACCURACY/ERROR
+	double result = _net->Train(values, size, output);
+	cout << result << endl;
 
 	delete values;
 }
