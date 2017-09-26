c95b0d7db758a24b8fd0a5d353304c5d74d9577a
Marc Rousavy <marcrousavy@hotmail.com>
2017-09-26 13:20:11 +0200

    Implement generic Vector to Array and vice versa functions

--- CppNeuralNetwork/Extensions.cpp
+++ CppNeuralNetwork/Extensions.cpp
@@ -1,26 +1,4 @@
 #include "stdafx.h"
 #include "Extensions.h"
 
-
-template<typedef T>
-inline T* Extensions::ToArray(std::vector<T> input)
-{
-	T* t = new T[input->size()];
-	for (int i = 0; i < input->size(); i++)
-	{
-		t[i] = input->at(i);
-	}
-	return t;
-}
-
-
-template<typedef T>
-inline std::vector<T> Extensions::ToVector(T* input, int length)
-{
-	std::vector<T> v = new std::vector<T>();
-	for (int i = 0; i < length; i++)
-	{
-		v.push_back(input[i]);
-	}
-	return t;
-}
+// IMPLEMENTATION IN EXTENSIONS_H (GENERIC TYPENAME T IMPL)
--- CppNeuralNetwork/Extensions.h
+++ CppNeuralNetwork/Extensions.h
@@ -5,9 +5,33 @@ class Extensions
 {
 public:
 	// Convert a vector to an array of type T
-	template <typedef T>
-	static T ToArray(std::vector<T> input);
+	template <typename T>
+	static T* ToArray(std::vector<T> input);
 	// Convert an array to a vector of type T
-	template <typedef T>
+	template <typename T>
 	static std::vector<T> ToVector(T* input, int length);
 };
+
+template<typename T>
+inline T* Extensions::ToArray(std::vector<T> input)
+{
+	T* t = new T[input->size()];
+	for (int i = 0; i < input->size(); i++)
+	{
+		t[i] = input->at(i);
+	}
+	return t;
+}
+
+
+template<typename T>
+inline std::vector<T> Extensions::ToVector(T* input, int length)
+{
+	std::vector<T> v = new std::vector<T>();
+	for (int i = 0; i < length; i++)
+	{
+		v.push_back(input[i]);
+	}
+	return t;
+}
+
