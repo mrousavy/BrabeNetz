b29aa6fa9591277c1be66a409cdbd573ba074397
mrousavy <mrousavy@donotreply.github.com>
2017-09-28 18:56:40 +0200

    Optimize Array -> Vector implementation

--- BrabeNetz/Extensions.h
+++ BrabeNetz/Extensions.h
@@ -6,31 +6,32 @@ class extensions
 public:
 	// Convert a vector to an array of type T
 	template <typename T>
-	static T* to_array(std::vector<T> input);
+	static T* to_array(std::vector<T>& input);
 	// Convert an array to a vector of type T
 	template <typename T>
 	static std::vector<T> to_vector(T* input, const int length);
+	// Convert an array to a vector of type T
+	template <typename T>
+	static std::vector<T> to_vector(T* input);
 };
 
 template <typename T>
-T* extensions::to_array(std::vector<T> input)
+T* extensions::to_array(std::vector<T>& input)
 {
-	T* t = new T[input->size()];
-	for (int i = 0; i < input->size(); i++)
-	{
-		t[i] = input->at(i);
-	}
-	return t;
+	return input.data(); // > C++ 11
 }
 
 
 template <typename T>
 std::vector<T> extensions::to_vector(T* input, const int length)
 {
-	std::vector<T> v;
-	for (int i = 0; i < length; i++)
-	{
-		v.push_back(input[i]);
-	}
+	std::vector<T> v(*input, length);
+	return v;
+}
+
+template <typename T>
+std::vector<T> extensions::to_vector(T* input)
+{
+	std::vector<int> v(input, input + sizeof input / sizeof input[0]);
 	return v;
 }
