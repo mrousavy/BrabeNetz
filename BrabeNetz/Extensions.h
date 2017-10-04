c706d6259dce0e281f74bdf1687f79a2454cdc93
Marc Rousavy <marcrousavy@hotmail.com>
2017-10-04 15:14:06 +0200

    Fix [] -> vector conversion

--- BrabeNetz/Extensions.h
+++ BrabeNetz/Extensions.h
@@ -10,9 +10,6 @@ public:
 	// Convert an array to a vector of type T
 	template <typename T>
 	static std::vector<T> to_vector(T* input, const int length);
-	// Convert an array to a vector of type T
-	template <typename T>
-	static std::vector<T> to_vector(T* input);
 };
 
 template <typename T>
@@ -25,13 +22,7 @@ T* extensions::to_array(std::vector<T>& input)
 template <typename T>
 std::vector<T> extensions::to_vector(T* input, const int length)
 {
-	std::vector<T> v(*input, length);
-	return v;
-}
-
-template <typename T>
-std::vector<T> extensions::to_vector(T* input)
-{
-	std::vector<int> v(input, input + sizeof input / sizeof input[0]);
+	std::vector<T> v;
+	v.assign(input, input + length);
 	return v;
 }
