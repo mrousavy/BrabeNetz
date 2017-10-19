c9c78190a8d2f490cd424a1ecbec7f4089fb617c
Marc Rousavy <marcrousavy@hotmail.com>
2017-10-19 10:02:43 +0200

    Implement basic reading from MNIST files

--- BrabeNetz/Trainer.cpp
+++ BrabeNetz/Trainer.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include "Trainer.h"
 #include <iostream>
+#include <fstream>
+#include <bitset>
+
+typedef bitset<8> byte; // byte
 
 void trainer::train_xor(network& net, const int train_times)
 {
@@ -49,7 +53,58 @@ void trainer::train_xor(network& net, const int train_times)
 	delete[] expected;
 }
 
-void trainer::train_handwritten_digits(network& net, int train_times, string mnist_filepath)
+void trainer::train_handwritten_digits(network& net, int train_times, const string mnist_images, const string mnist_labels)
 {
+	// Open streams
+	ifstream images_stream(mnist_images, fstream::in | fstream::binary); // Open the images file
+	ifstream labels_stream(mnist_labels, fstream::in | fstream::binary); // Open the labels file
 
+	if (!images_stream || !labels_stream) // Check if exists
+		throw "Images/Labels training file not found!";
+
+	// First 32 bit: MAGIC NUMBER
+	int i_magic_number = 0;
+	int l_magic_number = 0;
+	images_stream.read(reinterpret_cast<char*>(&i_magic_number), sizeof(i_magic_number));
+	labels_stream.read(reinterpret_cast<char*>(&l_magic_number), sizeof(l_magic_number));
+
+	// 32 bit: ELEMENTS COUNT
+	int images_count = 0;
+	int labels_count = 0;
+	images_stream.read(reinterpret_cast<char*>(&images_count), sizeof(images_count));
+	labels_stream.read(reinterpret_cast<char*>(&labels_count), sizeof(labels_count));
+
+	if (images_count != labels_count)
+		throw "Images count != Labels count - Aborting!";
+
+	// 2x 32 bit: IMAGE ROWS/COLUMNS (PIXELS)
+	int image_hpx = 0;
+	int image_vpx = 0;
+	images_stream.read(reinterpret_cast<char*>(&image_hpx), sizeof(image_hpx));
+	images_stream.read(reinterpret_cast<char*>(&image_vpx), sizeof(image_vpx));
+	const int pixels = image_hpx * image_vpx;
+
+	for (int i = 0; i < images_count; i++) // loop through each image/label
+	{
+		byte label;
+		labels_stream.read(reinterpret_cast<char*>(&label), sizeof(label)); // read 1 label
+
+		double* image = new double[pixels];
+		for (int p = 0; p < pixels; p++) // Loop through each pixel on this image
+		{
+			byte pixel;
+			labels_stream.read(reinterpret_cast<char*>(&pixel), sizeof(pixel)); // read 1 pixel
+			//image[p] = pixel;
+		}
+
+		double* total_error = new double();
+		//net.train(image, label, *total_error);
+
+		// cleanup
+		delete total_error;
+		delete[] image;
+	}
+
+	images_stream.close();
+	labels_stream.close();
 }
