992590a795d8b1f0f85a5ac03a0caa935cf4b6c8
mrousavy <mrousavy@donotreply.github.com>
2017-11-13 18:46:21 +0100

    Move extensions inline to a namespace

--- BrabeNetz/Extensions.h
+++ BrabeNetz/Extensions.h
@@ -1,26 +1,20 @@
 #pragma once
 #include <vector>
 
-class extensions
+namespace extensions
 {
-public:
 	// Convert a vector to an array of type T
 	template <typename T>
-	static T* to_array(std::vector<T>& input);
+	inline T* to_array(std::vector<T>& input)
+	{
+		return input.data(); // > C++ 11
+	}
+
+
 	// Convert an array to a vector of type T
 	template <typename T>
-	static std::vector<T> to_vector(T* input, const int length);
+	inline std::vector<T> to_vector(T* input, const int length)
+	{
+		return input.data(); // > C++ 11
+	}
 };
-
-template <typename T>
-T* extensions::to_array(std::vector<T>& input)
-{
-	return input.data(); // > C++ 11
-}
-
-
-template <typename T>
-std::vector<T> extensions::to_vector(T* input, const int length)
-{
-	return std::vector<T>(input, input + length);
-}
