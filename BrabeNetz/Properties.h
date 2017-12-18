142cffd61c024172de751434fa3aa9f57c7baec9
Marc Rousavy <marcrousavy@hotmail.com>
2017-12-18 11:23:05 +0100

    Add Properties network initializer struct
    This removes the compile time #defines :hash:

---
+++ BrabeNetz/Properties.h
@@ -0,0 +1,39 @@
+#pragma once
+
+/// \brief A structure for holding properties to initialize the network with
+struct properties
+{
+	/// \brief The default learn rate to begin with
+	double def_learn_rate;
+
+	/// \brief The minimum iterations a thread is
+	/// expected to do to start spawning multiple
+	/// threads on a backpropagation algorithm
+	int iters_per_thread;
+
+	/// \brief Force spawning multiple threads
+	/// on the backpropagation algorithm
+	/// (effectively ignores `iters_per_thread`)
+	bool force_multithreaded;
+
+	/// \brief The state file path to save the
+	/// network's save on `network::save()` to
+	char* state_file;
+
+	explicit properties(const double learn_rate = 0.01,
+	                    const int iters_per_thread = 600,
+	                    const bool force_multithreaded = false,
+						char* state_file = "state.nn")
+		:	def_learn_rate(learn_rate), 
+			iters_per_thread(iters_per_thread),
+			force_multithreaded(force_multithreaded),
+			state_file(state_file)
+	{
+		
+	}
+
+	~properties()
+	{
+		
+	}
+};
