8b3b4afac86ca67f17e11740e2eee3c765435bce
mrousavy <mrousavy@donotreply.github.com>
2018-01-18 21:40:11 +0100

    Build for x64 and x86 in build.bat

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -1,4 +1,6 @@
 @echo off
-msbuild ..\BrabeNetz\BrabeNetz.vcxproj /p:Configuration=Release
-msbuild ..\BrabeNetz\BrabeNetz.vcxproj /p:Configuration=Debug
+msbuild ..\BrabeNetz\BrabeNetz.vcxproj /p:Configuration=Release /p:Platform=x64
+msbuild ..\BrabeNetz\BrabeNetz.vcxproj /p:Configuration=Release /p:Platform=x86
+msbuild ..\BrabeNetz\BrabeNetz.vcxproj /p:Configuration=Debug /p:Platform=x64
+msbuild ..\BrabeNetz\BrabeNetz.vcxproj /p:Configuration=Debug /p:Platform=x86
 D:\nuget.exe pack BrabeNetz.nuspec
