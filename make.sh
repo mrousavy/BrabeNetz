99b5a23997eb4522358bf1b434a3c310a2a9ae7d
Marc Rousavy <marcrousavy@hotmail.com>
2018-02-01 11:21:09 +0100

    Make output file in script

--- make.sh
+++ make.sh
@@ -3,10 +3,13 @@
 echo "### BUILDING LIBRARY ##"
 cd BrabeNetz
 cmake -D CMAKE_BUILD_TYPE=Release CMakeLists.txt
+make
 cd ..
 echo "# BUILDING EXECUTABLE #"
 cd BrabeNetzConsole
 cmake -D CMAKE_BUILD_TYPE=Release CMakeLists.txt
+make
 cd ..
 echo "######## DONE #########"
+echo "Output File is located at $PWD/BrabeNetzConsole/bin/"
 
